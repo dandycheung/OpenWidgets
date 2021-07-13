ada13422064e6eefdb74b1aa6f4c3e80e95ae634
fazziclay <fazziclay@gmail.com>
2021-07-13 14:51:49 +0300

    rewritten

--- app/src/main/java/ru/fazziclay/openwidgets/activity/AboutActivity.java
+++ app/src/main/java/ru/fazziclay/openwidgets/activity/AboutActivity.java
@@ -1,45 +1,61 @@
 package ru.fazziclay.openwidgets.activity;
 
-import android.content.pm.PackageInfo;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import ru.fazziclay.openwidgets.R;
-import ru.fazziclay.openwidgets.updateChecker.UpdateChecker;
-import ru.fazziclay.openwidgets.widgets.data.WidgetsData;
 
 public class AboutActivity extends AppCompatActivity {
-    protected void onCreate(Bundle savedInstanceState) {
+    private static final String ABOUT_AUTHOR_TEXT = (
+            "Author:" + "\n" +
+            " - fazziclay@gmail.com" + "\n" +
+            " - https://fazziclay.ru"
+    );
+    private static final String ABOUT_DONATE_TEXT = (
+                    " Donate:" + "\n" +
+                    " - https://fazziclay.ru/donate"
+    );
+    private static final String ABOUT_APP_INFO_TEXT = (
+                    "App Info:" + "\n" +
+                    " - Dev..." // TODO: 13.07.2021 replace 'DEV' to app info
+    );
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final Context context = this;
         setTitle(R.string.activityTitle_about);
 
-        String appVersionName;
-        int appVersionBuild = -1;
-        try {
-            PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
-            appVersionName = pInfo.versionName;
-            appVersionBuild = pInfo.versionCode;
-        } catch (Exception e) {
-            appVersionName = "Error: " + e.toString();
-        }
+        final LinearLayout CONTENT = new LinearLayout(context);
+        final ScrollView backgroundScroll = new ScrollView(context);
+        final LinearLayout background = new LinearLayout(context);
 
-        TextView textView = new TextView(this);
-        textView.setPadding(16, 16, 16, 16);
-        textView.setText(("Author:\n" +
-                " - fazziclay@gmail.com\n" +
-                " - https://fazziclay.ru\n" +
-                "\n" +
-                " Donate:\n" +
-                " - https://fazziclay.ru/donate\n" +
-                "\n" +
-                "App Info:\n" +
-                " - updateCheckerAppBuild: " + UpdateChecker.appBuild + "\n - androidAppVersionBuild: " + appVersionBuild + "\n - androidAppVersionName: " + appVersionName + "\n - widgetDataVersion: " + WidgetsData.version+
-                "\n\n\n\n"+
-                "Github: http://github.com/fazziclay/openwidgets/"));
+        CONTENT.addView(backgroundScroll);
+        backgroundScroll.addView(background);
+        background.setOrientation(LinearLayout.VERTICAL);
 
-        addContentView(textView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        addContentView(CONTENT, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+
+        final TextView aboutAuthorTextView = new TextView(context);
+        final TextView aboutDonateTextView = new TextView(context);
+        final TextView aboutAppInfoTextView = new TextView(context);
+
+        aboutAuthorTextView.setPadding(16, 16, 16, 16);
+        aboutDonateTextView.setPadding(16, 16, 16, 16);
+        aboutAppInfoTextView.setPadding(16, 16, 16, 16);
+
+        aboutAuthorTextView.setText(ABOUT_AUTHOR_TEXT);
+        aboutDonateTextView.setText(ABOUT_DONATE_TEXT);
+        aboutAppInfoTextView.setText(ABOUT_APP_INFO_TEXT);
+
+        background.addView(aboutAuthorTextView);
+        background.addView(aboutDonateTextView);
+        background.addView(aboutAppInfoTextView);
     }
 }
