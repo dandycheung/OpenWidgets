5d6259568e555328e11d5c0605b2d1337bd25719
fazziclay <fazziclay@gmail.com>
2021-07-16 17:26:45 +0300

    навертел я тут конечно...

--- app/src/main/java/ru/fazziclay/openwidgets/UpdateChecker.java
+++ app/src/main/java/ru/fazziclay/openwidgets/UpdateChecker.java
@@ -1,131 +1,150 @@
 package ru.fazziclay.openwidgets;
 
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
-
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
-
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-import ru.fazziclay.openwidgets.android.activity.MainActivity;
-import ru.fazziclay.openwidgets.deprecated.cogs.DeprecatedUtils;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import ru.fazziclay.fazziclaylibs.InternetUtils;
+import ru.fazziclay.fazziclaylibs.JSONUtils;
 
 
 public class UpdateChecker {
-    public static int appBuild = 5;
-    public static int appUpdateCheckerFormatVersion = 1;
-    public static String appVersionsUrl = "https://raw.githubusercontent.com/FazziCLAY/OpenWidgets/master/app_versions.json";
+    public static int APP_BUILD = 6;
+    public static int APP_UPDATE_CHECKER_FORMAT_VERSION = 3;
+    public static String APP_VERSIONS_URL = "https://raw.githubusercontent.com/FazziCLAY/OpenWidgets/master/app_versions_test.json";
+    public static final boolean LOCAL_APP_VERSIONS = true;
+
+    static ArrayList<Version> versionsHistory = null;
+    static Version lastRelease = null;
+
+    static String parsed = "{1}";
+    public static JSONObject parsedJSON = null;
+    static Status status = Status.VARIABLE_NOT_SET;
+    static int lastUpdateCheckerFormatVersion = -1000;
+    static boolean isLastUpdateCheckerFormatVersionSupported = true;
+
+
+    private static Version getLatestRelease() {
+        int i = 0;
+        while (i < versionsHistory.size()) {
+            Version version = versionsHistory.get(i);
+            if (version.type == VersionType.RELEASE) {
+                return version;
+            }
+            i++;
+        }
+
+        return null;
+    }
+
+    private static Version getVersionByBuild(int build) {
+        int i = 0;
+        while (i < versionsHistory.size()) {
+            Version version = versionsHistory.get(i);
+            if (version.build == build) {
+                return version;
+            }
+            i++;
+        }
+
+        return null;
+    }
+
+    private static void parse() throws IOException {
+        if (!LOCAL_APP_VERSIONS) {
+            parsed = InternetUtils.parseTextPage(APP_VERSIONS_URL);
+        }
+    }
+
+    private static void parseJSON() throws JSONException {
+        isLastUpdateCheckerFormatVersionSupported = true;
+        parsedJSON = new JSONObject(parsed);
+        lastUpdateCheckerFormatVersion = (int) JSONUtils.get(parsedJSON, "format_version", -1000);
+
+        if (lastUpdateCheckerFormatVersion == 1) { // migrate to 3 format version
+            lastRelease = new Version(APP_BUILD, null, VersionType.RELEASE, null, null);
+
+        } else if (lastUpdateCheckerFormatVersion == APP_UPDATE_CHECKER_FORMAT_VERSION) {
+            versionsHistory = new ArrayList<>();
+            JSONArray versionsHistoryJson = parsedJSON.getJSONArray("versions_history");
+            int i = 0;
+            while (i < versionsHistoryJson.length()) {
+                versionsHistory.add(Version.fromJSON(versionsHistoryJson.getJSONObject(i)));
+                i++;
+            }
+
+            lastRelease = getLatestRelease();
+
+        } else {
+            isLastUpdateCheckerFormatVersionSupported = false;
+        }
+    }
 
     public static void getVersion(UpdateCheckerInterface versionInterface) {
+        final Logger LOGGER = new Logger(UpdateChecker.class, "getVersion");
         Thread updateCheckerThread = new Thread(() -> {
             try {
-                JSONObject a = new JSONObject(DeprecatedUtils.getPage(appVersionsUrl));
+                parse();
+                parseJSON();
+                LOGGER.log("Parsed successful");
 
-                //JSONObject a = new JSONObject();
-                //a.put("format_version", 1);
-                //JSONObject last1 = new JSONObject();
-                //last1.put("build", 5);
-                //last1.put("name", "TEST NAME");
-                //last1.put("download_url", "https://fazziclay.ru/");
-                //a.put("last", last1);
-
-
-                int formatVersion = a.getInt("format_version");
-                int lastBuild = 0;
-                String lastName = null;
-                String lastDownloadUrl = null;
-
-                if (formatVersion == appUpdateCheckerFormatVersion) {
-                    JSONObject last = a.getJSONObject("last");
-                    lastBuild = last.getInt("build");
-                    lastName = last.getString("name");
-                    lastDownloadUrl = last.getString("download_url");
-                }
-
-                // -1 - Текущая версия выше самой последней версии
-                // 0 - Текущая версия последняя.
-                // 1 - Имеется обновление
-                // 2 - Format Version не такой какой понимает это приложение
-                // 3 - Ошибка
-                byte status = 0;
-                if (formatVersion != appUpdateCheckerFormatVersion) {
-                    status = 2;
-                } else if (lastBuild > appBuild) {
-                    status = 1;
-                } else if (lastBuild < appBuild) {
-                    status = -1;
-                }
-
-                versionInterface.run(status, lastBuild, lastName, lastDownloadUrl);
-            } catch (Exception e) {
-                versionInterface.run((byte) 3, 0, e.toString(), null);
+            } catch (IOException | JSONException exception) {
+                status = Status.PARSING_ERROR;
+                LOGGER.error("Parsing error.");
+                LOGGER.exception(exception);
+                LOGGER.log("Sent this error to versionInterface; status=" + status);
+                versionInterface.run(status, lastRelease, exception);
+                return;
             }
+
+            if (!isLastUpdateCheckerFormatVersionSupported) {
+                status = Status.FORMAT_VERSION_NOT_SUPPORTED;
+
+            } else if (versionsHistory.size() == 0) {
+                status = Status.VERSIONS_HISTORY_EMPTY;
+
+            } else if (getVersionByBuild(APP_BUILD) == null) {
+                status = Status.VERSION_UNKNOWN;
+
+            } else if (lastRelease == null) {
+                status = Status.LATEST_RELEASE_NOT_FOUND;
+
+            } else if (APP_BUILD == lastRelease.getBuild()) {
+                status = Status.VERSION_LATEST;
+
+            } else if (APP_BUILD < lastRelease.getBuild()) {
+                status = Status.VERSION_OUTDATED;
+
+            } else if (APP_BUILD > lastRelease.getBuild()) {
+                status = Status.VERSION_NOT_RELEASE;
+            }
+
+            LOGGER.log("run interface: status=" + status);
+            versionInterface.run(status, lastRelease, null);
         });
 
+        LOGGER.log("started updateCheckerThread, updateCheckerThread.getName()="+updateCheckerThread.getName());
         updateCheckerThread.start();
     }
 
-    public static void sendNewUpdateAvailableNotification(Context context) {
-        UpdateChecker.getVersion((status, build, name, downloadUrl) -> {
-            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
 
-            if (status == 0) {
-                if (DeprecatedUtils.isNotifyShowed(context, 200)) notificationManager.cancel(200);
-                if (DeprecatedUtils.isNotifyShowed(context, 201)) notificationManager.cancel(201);
-                if (DeprecatedUtils.isNotifyShowed(context, 202)) notificationManager.cancel(202);
 
-            } else {
-                PendingIntent openAppIntent = PendingIntent.getActivity(context, 1, new Intent(context, MainActivity.class), 0);
-                PendingIntent toSiteIntent = PendingIntent.getActivity(context, 1, new Intent(Intent.ACTION_VIEW, Uri.parse("http://github.com/fazziclay/openwidgets/releases")), 0);
-
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "UpdateChecker")
-                        .setSmallIcon(R.mipmap.ic_launcher)
-                        .setPriority(NotificationCompat.PRIORITY_HIGH)
-                        .setContentIntent(openAppIntent);
-
-                if (status == 1 && !DeprecatedUtils.isNotifyShowed(context, 200)) {
-                    PendingIntent downloadIntent = PendingIntent.getActivity(context, 1, new Intent(Intent.ACTION_VIEW, Uri.parse(downloadUrl)), 0);
-                    builder = builder
-                            .setContentText(context.getText(R.string.updateChecker_updateAvailable))
-                            .addAction(R.mipmap.ic_launcher, context.getString(R.string.updateChecker_button_toSite), toSiteIntent)
-                            .addAction(R.mipmap.ic_launcher, context.getString(R.string.updateChecker_button_download), downloadIntent);
-
-                    notificationManager.notify(200, builder.build());
-                }
-
-                if (status == 2 && !DeprecatedUtils.isNotifyShowed(context, 201)) {
-                    builder = builder
-                            .setContentText(context.getText(R.string.updateChecker_newFormatVersion))
-                            .addAction(R.mipmap.ic_launcher, context.getString(R.string.updateChecker_button_toSite), toSiteIntent);
-
-                    notificationManager.notify(201, builder.build());
-                }
-
-                if (status == 2 && !DeprecatedUtils.isNotifyShowed(context, 201)) {
-                    builder = builder
-                            .setContentText(context.getText(R.string.updateChecker_newFormatVersion))
-                            .addAction(R.mipmap.ic_launcher, context.getString(R.string.updateChecker_button_toSite), toSiteIntent);
-
-                    notificationManager.notify(201, builder.build());
-                }
-
-                if (status == -1 && !DeprecatedUtils.isNotifyShowed(context, 202)) {
-                    PendingIntent downloadIntent = PendingIntent.getActivity(context, 1, new Intent(Intent.ACTION_VIEW, Uri.parse(downloadUrl)), 0);
-                    builder = builder
-                            .setContentText(context.getText(R.string.updateChecker_versionHight))
-                            .addAction(R.mipmap.ic_launcher, context.getString(R.string.updateChecker_button_toSite), toSiteIntent)
-                            .addAction(R.mipmap.ic_launcher, context.getString(R.string.updateChecker_button_download), downloadIntent);
-
-                    notificationManager.notify(202, builder.build());
-                }
-            }
-        });
+    public interface UpdateCheckerInterface {
+        void run(Status status, Version latestRelease, Exception exception);
     }
 
-    public static interface UpdateCheckerInterface {
-        void run(byte status, int build, String name, String downloadUrl);
+    public enum Status {
+        FORMAT_VERSION_NOT_SUPPORTED,
+        VERSIONS_HISTORY_EMPTY,
+        LATEST_RELEASE_NOT_FOUND,
+        VARIABLE_NOT_SET,
+        PARSING_ERROR,
+        VERSION_OUTDATED,
+        VERSION_LATEST,
+        VERSION_UNKNOWN,
+        VERSION_NOT_RELEASE
     }
 }
