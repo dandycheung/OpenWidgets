6af366a2fde6973dbeb3b1cd9db12c2e8b72bf31
fazziclay <fazziclay@gmail.com>
2021-07-18 20:11:06 +0300

    recreated appWidget (date)

---
+++ app/src/main/java/ru/fazziclay/openwidgets/android/widget/DateAppWidget.java
@@ -0,0 +1,40 @@
+package ru.fazziclay.openwidgets.android.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+import ru.fazziclay.openwidgets.android.service.WidgetsUpdaterService;
+import ru.fazziclay.openwidgets.data.widgets.WidgetsData;
+import ru.fazziclay.openwidgets.data.widgets.widget.DateWidget;
+
+
+public class DateAppWidget extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            if (WidgetsData.getWidgetsData().getDateWidgetById(appWidgetId) == null) {
+                WidgetsData.getWidgetsData().getDateWidgets().add(new DateWidget(appWidgetId));
+            }
+        }
+
+        WidgetsData.save();
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            DateWidget dateWidget = WidgetsData.getWidgetsData().getDateWidgetById(appWidgetId);
+            if (dateWidget != null) {
+                dateWidget.delete();
+            }
+        }
+
+        WidgetsData.save();
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        WidgetsUpdaterService.stop(context);
+    }
+}
