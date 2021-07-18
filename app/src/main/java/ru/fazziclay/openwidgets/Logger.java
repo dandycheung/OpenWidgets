badc2d109c1cbf14d2075e9211a77c0f1ef5f097
fazziclay <fazziclay@gmail.com>
2021-07-18 20:12:43 +0300

    LOGGER!

---
+++ app/src/main/java/ru/fazziclay/openwidgets/Logger.java
@@ -0,0 +1,63 @@
+package ru.fazziclay.openwidgets;
+
+import android.util.Log;
+
+import ru.fazziclay.fazziclaylibs.FileUtils;
+import ru.fazziclay.openwidgets.data.Paths;
+import ru.fazziclay.openwidgets.util.ExceptionUtils;
+import ru.fazziclay.openwidgets.util.TimeUtils;
+
+public class Logger {
+    public static final String LOG_FILE = "debug/log.txt";
+
+    String calledInFile;
+    String function = null;
+
+    public Logger(Class calledFrom) {this.calledInFile = calledFrom.getSimpleName();}
+    public Logger(Class calledFrom, String function) {
+        this.calledInFile = calledFrom.getSimpleName();
+        this.function = function;
+
+        function();
+    }
+
+    public void setFunction(String function) {
+        this.function = function;
+        function();
+    }
+
+    public void clear() {
+        FileUtils.write(Paths.appFilePath + "/" + Logger.LOG_FILE, "");
+        info("Logs cleared");
+    }
+
+    public void info(String message) {raw("INFO", message);}
+    public void log(String message) {raw("LOG", message);}
+    public void error(String message) {raw("ERROR", message);}
+    public void exception(Exception exception) {
+        raw("Exception", exception.toString() + "\n-- StackTrace --\n" + ExceptionUtils.getStackTrace(exception) + "-- StackTrace end --");
+    }
+    public void debug(String message) {raw("DEBUG", message);}
+    public void function() {raw("FUNCTION_CALLED", "Called!");}
+    public void functionReturned(String function, Object returned) {raw("FUNCTION_RETURNED", "function "+function+" returned "+returned.toString());}
+    public void returned(String message) {
+        raw("RETURNED", message);
+    }
+
+    public void returnedThrow(String message) {
+        raw("RETURNED_THROW", message);
+    }
+
+    public void returnedThrow(Exception exception) {
+        raw("RETURNED_THROW", exception.toString());
+    }
+
+    private void raw(String tag, String message) {
+        String logPath = Paths.appFilePath+"/"+ LOG_FILE;
+        String s = String.format("[%s %s %s] %s", TimeUtils.dateFormat("%H:%M:%S"), tag, this.calledInFile, message);
+        if (function != null) s = String.format("[%s %s %s.%s] %s", TimeUtils.dateFormat("%H:%M:%S"), tag, this.calledInFile, function, message);
+
+        Log.d(tag, s);
+        FileUtils.write(logPath, FileUtils.read(logPath) + "\n" + s);
+    }
+}
---
+++ app/src/main/java/ru/fazziclay/openwidgets/android/activity/LoggerActivity.java
@@ -0,0 +1,76 @@
+package ru.fazziclay.openwidgets.android.activity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import ru.fazziclay.fazziclaylibs.FileUtils;
+import ru.fazziclay.openwidgets.Logger;
+import ru.fazziclay.openwidgets.R;
+import ru.fazziclay.openwidgets.data.Paths;
+
+public class LoggerActivity extends AppCompatActivity {
+    ScrollView scrollView;
+    LinearLayout background;
+    Button clearButton;
+    Button textSizePlus;
+    Button textSizeMinus;
+    CheckBox autoScrollCheckBox;
+    TextView logText;
+
+    float textSize = 10.0f;
+
+    private void loadVariables() {
+        scrollView = findViewById(R.id.logger_scroll);
+        background = findViewById(R.id.logger_background);
+        clearButton = findViewById(R.id.logger_clear);
+        textSizePlus = findViewById(R.id.logger_textSizePlus);
+        textSizeMinus = findViewById(R.id.logger_textSizeMinus);
+        autoScrollCheckBox = findViewById(R.id.logger_autoScroll);
+        logText = new TextView(this);
+
+        // Setting
+        background.addView(logText);
+    }
+
+    private void loadLogic() {
+        final Logger LOGGER = new Logger(LoggerActivity.class, "loadVariables");
+        clearButton.setOnClickListener(v -> LOGGER.clear());
+        textSizePlus.setOnClickListener(v -> textSize = textSize + 1.0f);
+        textSizeMinus.setOnClickListener(v -> textSize = textSize - 1.0f);
+    }
+
+    private void loadTextUpdater() {
+        final Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                logText.setText(FileUtils.read(Paths.appFilePath + "/" + Logger.LOG_FILE));
+                logText.setTextSize(textSize);
+                if (autoScrollCheckBox.isChecked()) scrollView.scrollTo(0, 999999999);
+
+                if (!isFinishing()) {
+                    handler.postDelayed(this, 100);
+                }
+            }
+        };
+        handler.post(runnable);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logger);
+        setTitle(R.string.activityTitle_logger);
+
+        loadVariables();
+        loadLogic();
+        loadTextUpdater();
+    }
+}
