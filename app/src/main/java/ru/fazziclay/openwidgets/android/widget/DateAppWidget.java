dde4fee77e31840335f5e0f2056183df1497ef32
fazziclay <fazziclay@gmail.com>
2021-07-23 17:12:58 +0300

    fix non app loaded before >> added widget error

--- app/src/main/java/ru/fazziclay/openwidgets/android/widget/DateAppWidget.java
+++ app/src/main/java/ru/fazziclay/openwidgets/android/widget/DateAppWidget.java
@@ -5,6 +5,7 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 
 import ru.fazziclay.openwidgets.android.service.WidgetsUpdaterService;
+import ru.fazziclay.openwidgets.data.Paths;
 import ru.fazziclay.openwidgets.data.widgets.WidgetsData;
 import ru.fazziclay.openwidgets.data.widgets.widget.DateWidget;
 
@@ -12,6 +13,9 @@ import ru.fazziclay.openwidgets.data.widgets.widget.DateWidget;
 public class DateAppWidget extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        Paths.updatePaths(context);
+        WidgetsData.load();
+
         for (int appWidgetId : appWidgetIds) {
             if (WidgetsData.getWidgetsData().getDateWidgetById(appWidgetId) == null) {
                 WidgetsData.getWidgetsData().getDateWidgets().add(new DateWidget(appWidgetId));
@@ -23,6 +27,9 @@ public class DateAppWidget extends AppWidgetProvider {
 
     @Override
     public void onDeleted(Context context, int[] appWidgetIds) {
+        Paths.updatePaths(context);
+        WidgetsData.load();
+
         for (int appWidgetId : appWidgetIds) {
             DateWidget dateWidget = WidgetsData.getWidgetsData().getDateWidgetById(appWidgetId);
             if (dateWidget != null) {
