ebdab003e0f6668294aa786d96023792110a762a
fazziclay <fazziclay@gmail.com>
2021-07-23 17:10:54 +0300

    Moved to FazziCLAYLibs.jar

--- app/src/main/java/ru/fazziclay/openwidgets/util/ByteUtils.java
+++
@@ -1,8 +0,0 @@
-package ru.fazziclay.openwidgets.util;
-
-public class ByteUtils {
-    public static String byteToHex(int value) {
-        String hex = "00".concat(Integer.toHexString(value));
-        return hex.substring(hex.length()-2);
-    }
-}
