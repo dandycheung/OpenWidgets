bbbc6f65b681ac84d226a28e4122289ec6f4fef1
fazziclay <fazziclay@gmail.com>
2021-08-07 22:19:41 +0300

    variables make private

--- app/src/main/java/ru/fazziclay/openwidgets/data/Paths.java
+++ app/src/main/java/ru/fazziclay/openwidgets/data/Paths.java
@@ -2,12 +2,28 @@ package ru.fazziclay.openwidgets.data;
 
 import android.content.Context;
 
+import java.io.File;
+
 public class Paths {
-    public static String appFilePath = null;
-    public static String appCachePath = null;
+    private static String appFilePath = null;
+    private static String appCachePath = null;
 
     public static void updatePaths(Context context) {
         appFilePath = context.getFilesDir().getPath();
         appCachePath = context.getCacheDir().getPath();
+    }
+
+    public static String getAppFilePath() {
+        if (appFilePath == null) {
+            return null;
+        }
+        return appFilePath + File.separator;
+    }
+
+    public static String getAppCachePath() {
+        if (appCachePath == null) {
+            return null;
+        }
+        return appCachePath + File.separator;
     }
 }
