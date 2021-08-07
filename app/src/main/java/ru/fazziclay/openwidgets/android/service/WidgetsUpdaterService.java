789a1a382a7dbd4d2f1f2810c44cf25baffa3e6f
fazziclay <fazziclay@gmail.com>
2021-08-07 22:20:31 +0300

    added powerKey events support! added logger

--- app/src/main/java/ru/fazziclay/openwidgets/android/service/WidgetsUpdaterService.java
+++ app/src/main/java/ru/fazziclay/openwidgets/android/service/WidgetsUpdaterService.java
@@ -1,8 +1,10 @@
 package ru.fazziclay.openwidgets.android.service;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
 import android.os.Handler;
 import android.os.IBinder;
@@ -22,22 +24,51 @@ import ru.fazziclay.openwidgets.util.Utils;
 
 
 public class WidgetsUpdaterService extends Service {
-    private static final Logger LOGGER = new Logger(WidgetsUpdaterService.class);
+    public static final String FOREGROUND_NOTIFICATION_CHANNEL_ID = "WidgetsUpdaterServiceForeground";
+    public static final int FOREGROUND_NOTIFICATION_ID = 100;
     public static boolean isRun;
+    static BroadcastReceiver mPowerKeyReceiver = null;
+
+    private void registerPowerKeyReceiver() {
+        final Logger LOGGER = new Logger(WidgetsUpdaterService.class, "registerPowerKeyReceiver");
+
+        final IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_SCREEN_ON);
+        intentFilter.addAction(Intent.ACTION_SCREEN_OFF);
+
+        mPowerKeyReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                LOGGER.log("action="+action);
+
+                if (action.equals(Intent.ACTION_SCREEN_ON)) {
+                    startIsNot(context);
+                }
+
+                if (action.equals(Intent.ACTION_SCREEN_OFF)) {
+                    stop(context);
+                }
+            }
+        };
+
+        getApplicationContext().registerReceiver(mPowerKeyReceiver, intentFilter);
+    }
 
     public static void stop(Context context) {
-        LOGGER.setFunction("stop");
+        final Logger LOGGER = new Logger(WidgetsUpdaterService.class, "stop");
         context.stopService(new Intent(context, WidgetsUpdaterService.class));
     }
 
     public static void startIsNot(Context context) {
-        LOGGER.setFunction("startIsNot");
+        final Logger LOGGER = new Logger(WidgetsUpdaterService.class, "startIsNot");
 
         if (!ServiceUtils.isServiceStarted(context, WidgetsUpdaterService.class)) {
-            LOGGER.log("started!");
             context.startService(new Intent(context, WidgetsUpdaterService.class));
+            LOGGER.log("started!");
+        } else {
+            LOGGER.log("before started");
         }
-        LOGGER.log("before started ;(");
     }
 
     private void loop() {
@@ -58,23 +89,26 @@ public class WidgetsUpdaterService extends Service {
     }
 
     public void onCreate() {
-        super.onCreate();
-        LOGGER.setFunction("onCreate");
-        isRun = true;
+        final Logger LOGGER = new Logger(WidgetsUpdaterService.class, "onCreate");
+        isRun = true; LOGGER.log("isRun = true");
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "WidgetsUpdaterServiceForeground")
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, FOREGROUND_NOTIFICATION_CHANNEL_ID)
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle(getText(R.string.notification_foregroundWidgetsUpdaterService_title))
                 .setContentText(getText(R.string.notification_foregroundWidgetsUpdaterService_text))
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .setSound(null)
                 .setAutoCancel(true);
 
-        startForeground(100, builder.build());
+        startForeground(FOREGROUND_NOTIFICATION_ID, builder.build());
+        registerPowerKeyReceiver();
+        super.onCreate();
     }
 
     public int onStartCommand(Intent intent, int flags, int startId) {
-        super.onStartCommand(intent, flags, startId);
         final Logger LOGGER = new Logger(WidgetsUpdaterService.class, "onStartCommand");
+
+        WidgetsData.load();
 
         final Context finalContext = this;
         final Handler handler = new Handler();
@@ -95,19 +129,19 @@ public class WidgetsUpdaterService extends Service {
             }
         };
 
-        WidgetsData.load();
         handler.post(runnable);
+        super.onStartCommand(intent, flags, startId);
         return START_STICKY;
     }
 
     public void onDestroy() {
         super.onDestroy();
-        LOGGER.setFunction("onDestroy");
-        isRun = false;
+        final Logger LOGGER = new Logger(WidgetsUpdaterService.class, "onDestroy");
+        isRun = false; LOGGER.log("isRun = false");
     }
 
     public IBinder onBind(Intent intent) {
-        LOGGER.setFunction("onBind");
+        final Logger LOGGER = new Logger(WidgetsUpdaterService.class, "onBind");
         LOGGER.returned(null);
         return null;
     }
