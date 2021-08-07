52b5681ee0f7c36aa3f8241a0d71a2f433023548
fazziclay <fazziclay@gmail.com>
2021-08-07 22:24:05 +0300

    added importance

--- app/src/main/java/ru/fazziclay/openwidgets/util/NotificationUtils.java
+++ app/src/main/java/ru/fazziclay/openwidgets/util/NotificationUtils.java
@@ -9,12 +9,19 @@ import android.service.notification.StatusBarNotification;
 import ru.fazziclay.openwidgets.Logger;
 
 public class NotificationUtils {
-    public static void createChannel(Context context, String channelId, String name, String description) {
+    public static final byte IMPORTANCE_LOW = 10;
+
+    public static void createChannel(Context context, String channelId, String name, String description, byte importance) {
         final Logger LOGGER = new Logger(NotificationUtils.class, "createChannel");
         LOGGER.log("channelId="+channelId+", name="+name+", description="+description);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(channelId, name, NotificationManager.IMPORTANCE_DEFAULT);
+            int i = NotificationManager.IMPORTANCE_DEFAULT;
+            if (importance == IMPORTANCE_LOW) {
+                i = NotificationManager.IMPORTANCE_LOW;
+            }
+
+            NotificationChannel channel = new NotificationChannel(channelId, name, i);
             channel.setDescription(description);
             NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
