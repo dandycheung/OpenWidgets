6c76e705033b87b07a9b745bb6630dc80e08b8d8
fazziclay <fazziclay@gmail.com>
2021-08-07 22:18:09 +0300

    new json utils!

---
+++ app/src/main/java/ru/fazziclay/openwidgets/util/JsonUtils.java
@@ -0,0 +1,17 @@
+package ru.fazziclay.openwidgets.util;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class JsonUtils {
+    public static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    public static final String EMPTY_JSON_CONTENT = "{}";
+
+    public static <T> T fromJson(String content, Class<T> typeOfT) {
+         return GSON.fromJson(content, typeOfT);
+    }
+
+    public static String toJson(Object src) {
+        return GSON.toJson(src);
+    }
+}
