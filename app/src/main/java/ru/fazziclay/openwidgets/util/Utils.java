6f70a28db7d679782f9c75c92c31aa71dfcae9b3
fazziclay <fazziclay@gmail.com>
2021-08-09 21:58:56 +0300

    share experiments continued

--- app/src/main/java/ru/fazziclay/openwidgets/util/Utils.java
+++ app/src/main/java/ru/fazziclay/openwidgets/util/Utils.java
@@ -9,9 +9,8 @@ import android.widget.Toast;
 import java.io.File;
 import java.util.Locale;
 
-import ru.fazziclay.openwidgets.ErrorDetectorWrapper;
+import ru.fazziclay.fazziclaylibs.FileUtils;
 import ru.fazziclay.openwidgets.Logger;
-import ru.fazziclay.openwidgets.android.ContextSaver;
 import ru.fazziclay.openwidgets.data.Paths;
 
 public class Utils {
@@ -31,38 +30,32 @@ public class Utils {
         showToast(context, message, Toast.LENGTH_SHORT);
     }
 
-    public static void showToast(String message) {
-        ErrorDetectorWrapper.errorDetectorWrapper(() -> showToast(ContextSaver.getContext(), message, Toast.LENGTH_SHORT));
-    }
-
     public static void shareText(Context context, String title, String text) {
-        /*
-        Uri uri = Uri.parse("file://"+Paths.getAppFilePath() + Logger.LOG_FILE);
+
+        Uri uri = Uri.parse(Paths.getAppFilePath() + Logger.LOG_FILE);
 
         Intent intentShareFile = new Intent(Intent.ACTION_SEND);
-        intentShareFile.setType("*");
+        intentShareFile.setType("*/*");
         //intentShareFile.putExtra(Intent.EXTRA_TEXT, text);
         intentShareFile.putExtra(Intent.EXTRA_SUBJECT, "Debug for OpenWidgets app:");
         intentShareFile.putExtra(Intent.EXTRA_STREAM, uri);
-        //intentShareFile.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        context.startActivity(Intent.createChooser(intentShareFile, title));*/
-        shareFile(context, new File(Paths.getAppFilePath() + Logger.LOG_FILE));
+        intentShareFile.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        context.startActivity(Intent.createChooser(intentShareFile, title));
+        //shareFile(context,Paths.getAppFilePath() + Logger.LOG_FILE);
     }
 
-    private static void shareFile(Context context, File file) {
-        String filePath = file.getAbsolutePath();
-        File f = new File(filePath);
+    private static void shareFile(Context context, String filePath) {
+        String cachedFilePath = context.getExternalCacheDir().getAbsolutePath() + filePath;
+        FileUtils.write(cachedFilePath, FileUtils.read(filePath));
 
-        Intent intentShareFile = new Intent(Intent.ACTION_SEND);
-        File fileWithinMyDir = new File(filePath);
+        File file = new File(cachedFilePath);
+        Intent share = new Intent(Intent.ACTION_SEND);
+        share.setType("text/plain");
 
-        if (fileWithinMyDir.exists()) {
-            intentShareFile.setType("text/*");
-            intentShareFile.putExtra(Intent.EXTRA_STREAM, Uri.parse("file://" + filePath));
-            intentShareFile.putExtra(Intent.EXTRA_SUBJECT, "MyApp File Share: " + f.getName());
-            intentShareFile.putExtra(Intent.EXTRA_TEXT, "MyApp File Share: " + f.getName());
+        share.putExtra(Intent.EXTRA_STREAM,
+                Uri.parse(file.getAbsolutePath()));
+        //share.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
 
-            context.startActivity(Intent.createChooser(intentShareFile, f.getName()));
-        }
+        context.startActivity(Intent.createChooser(share, "Share"));
     }
 }
