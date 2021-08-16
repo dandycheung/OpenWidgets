62cf79cfeb2f140e5acfafa039dec20de256183d
fazziclay <fazziclay@gmail.com>
2021-08-16 19:42:47 +0300

    deleted deprecated ErrorDetectorWrapper.context

--- app/src/main/java/ru/fazziclay/openwidgets/ErrorDetectorWrapper.java
+++ app/src/main/java/ru/fazziclay/openwidgets/ErrorDetectorWrapper.java
@@ -1,22 +1,17 @@
 package ru.fazziclay.openwidgets;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Handler;
 
 import ru.fazziclay.openwidgets.util.Utils;
 
-@Deprecated
 public class ErrorDetectorWrapper {
-    @SuppressLint("StaticFieldLeak")
-    public static Context context;
-    @Deprecated
-    public static void errorDetectorWrapper(ErrorDetectorWrapperInterface errorDetectorWrapperInterface) {
+    public static void errorDetectorWrapper(Context context, ErrorDetectorWrapperInterface errorDetectorWrapperInterface) {
         try {
             errorDetectorWrapperInterface.run();
         } catch (Exception exception) {
             try {
-                new Handler().post(() -> Utils.showToast(context, "Error detected: " + exception.toString()));
+                new Handler().post(() -> Utils.showToast(context, "Error: " + exception.toString()));
                 final Logger LOGGER = new Logger();
                 LOGGER.error(exception);
             } catch (Exception ignored) {}
