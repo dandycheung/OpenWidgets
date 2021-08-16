54fbbdf2873663f31ab58dd2f31f42a4c975acd6
fazziclay <fazziclay@gmail.com>
2021-08-16 19:44:33 +0300

    flip .equals()

--- app/src/main/java/ru/fazziclay/openwidgets/android/receiver/AndroidBootCompleted.java
+++ app/src/main/java/ru/fazziclay/openwidgets/android/receiver/AndroidBootCompleted.java
@@ -9,8 +9,6 @@ import ru.fazziclay.openwidgets.android.service.WidgetsUpdaterService;
 public class AndroidBootCompleted extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
-            WidgetsUpdaterService.startIsNot(context);
-        }
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) WidgetsUpdaterService.startIsNot(context);
     }
 }
